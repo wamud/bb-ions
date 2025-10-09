5a07ca8885e5a6f70b4ef2087493bf0d3abc6152
Madhav Krishnan Vijayan <39261345+madhavkrishnan@users.noreply.github.com>
2025-10-09 13:42:26 +1100

    add BBCode class

--- src/bb_ions/bb_code.py
+++ src/bb_ions/bb_code.py
@@ -1,12 +1,16 @@
 """
-This module generates the parity check matrices for a bivariate-bycycle code
+This module defines the BBCode class which is used to generate check matrices 
+and logical operators for a bivariate-bycycle code.
 
 Ref: 
 """
 
 from typing import Iterable, Tuple
 import numpy as np
+import json
 from numpy.linalg import matrix_power
+from bposd.css import css_code
+from bposd.css_decode_sim import css_decode_sim
 
 def generate_matrix_polynomial(
         x: np.ndarray,
@@ -23,36 +27,126 @@ def generate_matrix_polynomial(
         for i, j in zip(*poly_pow)
     )
 
-def bb_check_matrices(
+
+class BBCode:
+    """
+    Defines a bivariate-bycycle code
+    """
+    
+    OSD_OPTIONS = {
+        "target_runs": 2000,
+        "xyz_error_bias": [1, 1, 1],
+        "bp_method": "minimum_sum",
+        "ms_scaling_factor": 0.05,
+        "osd_method": "osd_cs",
+        "osd_order": 4,
+        "channel_update": None,
+        "seed": 42,
+        "max_iter": 9,  
+        "error_bar_precision_cutoff": 1e-6,
+        "tqdm_disable": True,
+        "error_rate": 0.1
+    }
+
+    def __init__(
+        self,
         l: int,
         m: int,
-        left_powers: Iterable[Iterable[int]],
-        right_powers: Iterable[Iterable[int]]) -> Tuple[np.ndarray]:
-    """
-    Generates Hx and Hz for the BB code
+        left_pow: Iterable[Iterable[int]],
+        right_pow: Iterable[Iterable[int]],
+        estimate_distance = False
+    ):
+        """
+        args
+        ----
+        l: 
+        m: 
+        left_pow:
+        right_pow:
+        """
+        self.l = l
+        self.m = m
+        self.left_pow = left_pow
+        self.right_pow = right_pow
+        self.Hx, self.Hz = self.generate_check_mat()
+        self.N, self.K = self.generate_nk()
+        self.distance = None
+        if estimate_distance:
+            self.distance = self.estimate_distance()
 
-    args
+    def generate_check_mat(self):
+        """
+        Generates Hx and Hz for the BB code
 
-    Returns
-    -------
-    (Hx, Hz) the X and Z parity check matrices 
-    ----
-    """
-    # Identity matrix cyclically shifted by 1 column
-    s_l = np.roll(np.eye(l, dtype=int), 1,  axis=1)
+        args
+
+        Returns
+        -------
+        (Hx, Hz) the X and Z parity check matrices 
+        ----
+        """
+        # Identity matrix cyclically shifted by 1 column
+        s_l = np.roll(np.eye(self.l, dtype=int), 1,  axis=1)
+        
+        # generate variables
+        x = np.kron(s_l, np.eye(self.m, dtype=int))
+        y = np.kron(np.eye(self.m, dtype=int), s_l)
+        
+        # generate polynomial
+        A = generate_matrix_polynomial(x, y, self.left_pow) 
+        B = generate_matrix_polynomial(x, y, self.right_pow) 
+        
+        Hx = np.hstack((A, B))
+        Hz = np.hstack((B.T, A.T))
+        
+        # check that all stabilizer checks commute
+        assert not np.any((Hx @ Hz.T) % 2)
+        
+        return Hx, Hz 
     
-    # generate variables
-    x = np.kron(s_l, np.eye(m, dtype=int))
-    y = np.kron(np.eye(m, dtype=int), s_l)
+    def generate_nk(self):
+        code = css_code(hx=self.Hx, hz=self.Hz)
+        return code.N, code.K
+
+    def estimate_distance(self):
+        """
+        Estimate distance of the code
+        """
+        
+        if self.distance is not None:
+            return self.distance
+
+        bb5 = css_decode_sim(
+            hx=self.Hx, hz=self.Hz, **self.OSD_OPTIONS
+        )
+
+        results = json.loads(bb5.output_dict())
+
+        dmax = results["min_logical_weight"]
+        
+        self.distance = dmax
+        
+        return dmax
+
+    def generate_logical_ops(self):
+        pass
+
+    def __str__(self):
+        A = ' + '.join([f"x^{i} * y^{j}" for i, j in zip(*self.left_pow)])
+        B = ' + '.join([f"x^{i} * y^{j}" for i, j in zip(*self.right_pow)])
+        n = self.N
+        k = self.K
+
+        if self.distance is None:
+            d = '?'
+        else:
+            d = self.distance
+        
+        return '\n'.join(
+            [f"[[{n}, {k}, <={d}]]",
+             f"l = {self.l}",
+             f"m = {self.m}",
+             f"A = {A}",
+             f"B = {B}"]
+        )
     
-    # generate polynomial
-    A = generate_matrix_polynomial(x, y, left_powers) 
-    B = generate_matrix_polynomial(x, y, right_powers) 
-    
-    Hx = np.hstack((A, B))
-    Hz = np.hstack((B.T, A.T))
-    
-    # check that all stabilizer checks commute
-    assert not np.any((Hx @ Hz.T) % 2)
-    
-    return Hx, Hz 
--- tests/test_bb_code.py
+++ tests/test_bb_code.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from bb_ions.bb_code import bb_check_matrices, generate_matrix_polynomial
+from bb_ions.bb_code import generate_matrix_polynomial, BBCode
 #from bb_ions.bbparamfuncs import get_code_params
 
 class BBCodeTest(unittest.TestCase):
@@ -27,14 +27,15 @@ class BBCodeTest(unittest.TestCase):
         
         assert np.array_equal(p, p_targ)
     
-    def test_bb_check_matrixes(
+    def test_generate_check_mat(
         self,
         l=12,
         m=12,
         left_pow=((3, 0, 0), (0, 2, 7)),
         right_pow=((0, 1, 2), (3, 0, 0))
     ):
-        hx, hz = bb_check_matrices(l, m, left_pow, right_pow)
+        bb = BBCode(l, m, left_pow, right_pow)
+        hx, hz = bb.generate_check_mat()
 
        # aij = tuple(zip(*left_pow))
        # bij = tuple(zip(*right_pow))
@@ -43,6 +44,32 @@ class BBCodeTest(unittest.TestCase):
 
        # assert np.array_equal(code.Hx, hx)
        # assert np.array_equal(code.Hz, hz)
+    
+    def test_generate_nk(
+        self,
+        l=12,
+        m=12,
+        left_pow=((3, 0, 0), (0, 2, 7)),
+        right_pow=((0, 1, 2), (3, 0, 0)),
+        targ_n = 288,
+        targ_k = 12
+    ):
+        bb = BBCode(l, m, left_pow, right_pow)
+        
+        n, k = bb.generate_nk()
+        
+        assert n == targ_n
+        assert k == targ_k
 
+    def test_estimate_distance(
+        self,
+        l=12,
+        m=12,
+        left_pow=((3, 0, 0), (0, 2, 7)),
+        right_pow=((0, 1, 2), (3, 0, 0)),
+    ):
+        
+        bb = BBCode(l, m, left_pow, right_pow)
+        print(bb.estimate_distance())
 if __name__ == "__main__":
     unittest.main()
