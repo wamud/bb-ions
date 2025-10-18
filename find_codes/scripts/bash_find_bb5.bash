3425ce6c2a197cfa1c90efdeda30ae4007265409
wamud <nawaran1@outlook.com>
2025-10-18 12:13:58 +1100

    Added some [[120, 8, 8]] codes; changed bash file to skip future ones

--- find_codes/scripts/bash_find_bb5.bash
+++ find_codes/scripts/bash_find_bb5.bash
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 for ((l=6; l<=12; l++)); do
-    for ((m=3; m<=12; m++)); do
+    for ((m=3; m<=11; m++)); do
         n=$((2 * l * m))
 
         if (( n < 60 )); then
             min_k=4
-            min_d=7
+            min_d=8
         elif (( n >= 60 && n <= 80 )); then
             min_k=10
             min_d=6
-        elif (( n > 80 && n <= 120 )); then
+        elif (( n > 80 && n <= 100 )); then
             min_k=8
             min_d=8
+	elif (( n > 100 && n <= 120 )); then
+	    min_k=8
+	    min_d=9
         elif (( n > 120 && n <= 150 )); then
-            min_k=8
+            min_k=10
             min_d=10
         else
             min_k=10
