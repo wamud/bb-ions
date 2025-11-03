b107c1221449d0758ec34f5b937ae5119789f1a1
24510265 <u24510265@hpcnode01.itd.uts.edu.au>
2025-11-03 17:37:52 +1100

    added bb_sims directory to run sims on HPC

---
+++ bb_sims/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Remove all files from run in prep for another run.
+rm -f *.e*[0-9]
+rm -f *.o*[0-9]
---
+++ bb_sims/submit.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Run this as: qsub submit.sh
+
+#PBS -N collect_stats
+#PBS -l ncpus=1
+#PBS -l mem=1GB
+#PBS -l walltime=00:00:20
+
+#PBS -m abe
+#PBS -M anthony.orourke@student.uts.edu.au
+
+###################################
+# Setup any input files for the run
+###################################
+
+
+# Create a /scratch directory with a unique name.
+SCRATCH="/scratch/${USER}_${PBS_JOBID%.*}"
+mkdir -p ${SCRATCH}
+
+cd ${PBS_O_WORKDIR}
+
+# Copy your input files from there to the scratch directory you created above.
+if [ -f collected_stats.csv ]; then
+    cp collected_stats.csv ${SCRATCH}
+fi
+
+###############
+# Start the Job
+###############
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate bb_env
+
+cd ${SCRATCH}
+python3 ${PBS_O_WORKDIR}/collect_stats.py 
+
+#####################################################
+# Copy results back to your own directory and cleanup
+#####################################################
+
+mv ${SCRATCH}/collected_stats.csv ${PBS_O_WORKDIR}
+
+cd ${PBS_O_WORKDIR}
+rmdir ${SCRATCH}
+
