931f73b52db03da3e738ac26308f17ef6ba54622
Anthony O'Rourke <anthony.orourke@student.uts.edu.au>
2025-11-21 15:01:39 +1100

    added 72_12_6 relaybp circuit and readied python file to search for circuit-level distance of relaybp circuits

--- find_circuit_level_distance/find_relay_circuit_level_distance.py
+++ find_circuit_level_distance/find_relay_circuit_level_distance.py
@@ -9,13 +9,24 @@ from pysat.formula import WCNF
 
 gross_code = stim.Circuit.from_file("../circuits/relaybp_circuits/circuit=bicycle_bivariate_144_12_12_memory_Z,distance=12,rounds=12,error_rate=0.001,noise_model=uniform_circuit,basis=CX,A=x^3+y+y^2,B=y^3+x+x^2.stim")
 
-circuits = [, gross_code]
+code_72 = stim.Circuit.from_file("../circuits/relaybp_circuits/circuit=bicycle_bivariate_72_12_6_memory_Z,distance=6,rounds=6,error_rate=0.001,noise_model=uniform_circuit,basis=CX,A=x^3+y+y^2,B=y^3+x+x^2.stim")
+
+circuits = [code_72, gross_code]
 
 circuit = circuits[int(sys.argv[1])]
 
+if int(sys.argv[1]) == 0:
+    n = 72
+    k = 12 
+    d_max = 6
+elif int(sys.argv[1]) == 1:
+    n = 144
+    k = 12
+    d_max = 12
+else:
+    raise ValueError("usage: python3 find_relay_circuit_level_distance.py 0/1")
 
-
-msg = f"- [[{code.n}, {code.k}, {code.d_max}]]"
+msg = f"- [[{n}, {k}, {d_max}]]"
 print(msg)
 
 # # Generate maxSAT problem of the circuit's distance, that other tools can solve.
@@ -34,8 +45,8 @@ with RC2(wcnf) as rc2:
 
     print(f"     {msg} : {msg2}")
 
-    with open("circ_distance_distance.txt", "a") as file:
+    with open("relay_circ_distance_distance.txt", "a") as file:
         file.write(f"{msg} : {msg2}\n")
-    with open("circ_distance_solution.txt", "a") as file:
+    with open("relay_circ_distance_solution.txt", "a") as file:
         file.write(f"{msg}\n   {msg1}\n")
 
