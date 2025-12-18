c1cedb25e91ca04c544d77163b183ff2eac57516
24510265 <u24510265@hpcnode01.itd.uts.edu.au>
2025-12-18 17:43:37 +1100

    added little experiment for testing

---
+++ bb_hpc_sims/little_submit.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Run this as: qsub submit.sh
+
+#PBS -N sims
+#PBS -l ncpus=4
+#PBS -l mem=50GB
+#PBS -l walltime=00:10:00
+#PBS -m abe
+#PBS -M anthony.orourke@student.uts.edu.au
+
+###################################
+# Setup any input files for the run
+###################################
+
+
+# Create a /scratch directory with a unique name.
+SCRATCH="/scratch/${USER}_${PBS_JOBID%.*}"
+mkdir -p ${SCRATCH}
+
+cd ${PBS_O_WORKDIR}
+
+# Copy your input files from there to the scratch directory you created above.
+if [ -d "../circuits/helios/exclude_opp_basis_detectors/288_code_copy/" ]; then
+    cp -r "../circuits/helios/exclude_opp_basis_detectors/288_code_copy/" "${SCRATCH}"
+fi
+
+
+###############
+# Start the Job
+###############
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate bb_env
+
+cd ${SCRATCH}
+python3 ${PBS_O_WORKDIR}/little_run_mem_exp.py
+
+#####################################################
+# Copy results back to your own directory and cleanup
+#####################################################
+
+mv ${SCRATCH}/*.csv ${PBS_O_WORKDIR}/collected_stats
+
+cd ${PBS_O_WORKDIR}
+rm -rf ${SCRATCH}
+
