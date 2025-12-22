ebf871072c6c4c103c7462835dbec188d863cfb0
Anthony O'Rourke <anthony.orourke@student.uts.edu.au>
2025-12-22 18:46:13 +1100

    minor edits

--- scripts/helios_run_mem_exp.py
+++ scripts/helios_run_mem_exp.py
@@ -13,11 +13,11 @@ def main():
     start_time = time.time()
     
 
-    circuit_paths = glob.glob(f"../circuits/helios/exclude_opp_basis_detectors/*.stim")
+    circuit_paths = glob.glob(f"../circuits/helios/exclude_opp_basis_detectors/360_code/*.stim")
     circuit_paths.sort()
 
     
-    csv_path = f"../collected_stats/helios_noise_long_chain_BPOSD_settings.csv"
+    csv_path = f"../collected_stats/helios_noise/helios_360.csv"
     # existing = [f"../collected_stats/tham_modules_noise_long_chain_BPOSD_settings_incl_opp_detectors.csv"]
 
     tasks = [
@@ -27,28 +27,6 @@ def main():
         )
         for path in circuit_paths
     ]
-
-
-    samples = sinter.collect(
-        num_workers = 64,
-        max_shots = 1000,
-        max_errors = 100,
-        tasks = tasks,
-        decoders=['bposd'],
-        # existing_data_filepaths = existing,
-        save_resume_filepath = csv_path,
-        custom_decoders = {
-            "bposd": SinterDecoder_BPOSD(
-                max_bp_iters = 10_000, # default 30
-                bp_method = "min_sum", # product_sum (default), min_sum, min_sum_log
-                # ms_scaling_factor = 0.625, # normalisation
-                # schedule = "serial", 
-                osd_method = "osd_cs", # "osd0" - zero-order OSD, "osd_e" - exhaustive OSD, "osd_cs": combination-sweep OSD (default)
-                osd_order = 5 
-            )
-        },
-        print_progress = True
-        )
 
 
     samples = sinter.collect(
@@ -69,7 +47,7 @@ def main():
                 osd_order = 5 
             )
         },
-        print_progress = False
+        print_progress = True
         )
 
     samples = sinter.collect(
