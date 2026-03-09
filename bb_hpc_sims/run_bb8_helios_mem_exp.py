69078380c785e3b8548320a6a460a3459cb3450b
wamud <anthony.orourke@student.uts.edu.au>
2026-03-09 22:19:55 +1100

    minor edits

--- bb_hpc_sims/run_bb8_helios_mem_exp.py
+++ bb_hpc_sims/run_bb8_helios_mem_exp.py
@@ -59,7 +59,7 @@ def main():
     samples = sinter.collect(
         num_workers = 32,
         max_shots = 10_000,
-        max_errors = 100,
+        max_errors = 50,
         tasks = tasks,
         decoders=['bposd'],
         save_resume_filepath = csv_path,
@@ -79,7 +79,7 @@ def main():
     samples = sinter.collect(
         num_workers = 32,
         max_shots = 100_000,
-        max_errors = 100,
+        max_errors = 50,
         tasks = tasks,
         decoders=['bposd'],
         save_resume_filepath = csv_path,
@@ -100,6 +100,26 @@ def main():
     samples = sinter.collect(
         num_workers = 32,
         max_shots = 1_000_000,
+        max_errors = 10,
+        tasks = tasks,
+        decoders=['bposd'],
+        save_resume_filepath = csv_path,
+        custom_decoders = {
+            "bposd": SinterDecoder_BPOSD(
+                ## Long chains settings:
+                max_bp_iters = 10_000,
+                bp_method = "min_sum",
+                osd_order = 5,
+                osd_method = "osd_cs"
+                # (other settings left unspecified so they take the default values)
+            )
+        },
+        print_progress = False
+        )
+
+    samples = sinter.collect(
+        num_workers = 32,
+        max_shots = 10_000_000,
         max_errors = 10,
         tasks = tasks,
         decoders=['bposd'],
