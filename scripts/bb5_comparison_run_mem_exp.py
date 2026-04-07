87dc880309556df0601d3c67a16f86c3f6791c90
Anthony O'Rourke <anthony.orourke@student.uts.edu.au>
2026-04-08 08:32:03 +1000

    plotted BB5 comparison, set to run more shots for 48,4,7

--- scripts/bb5_comparison_run_mem_exp.py
+++ scripts/bb5_comparison_run_mem_exp.py
@@ -79,8 +79,8 @@ def main():
 
     samples = sinter.collect(
         num_workers = 64,
-        max_shots = 1_000_000,
-        max_errors = 100,
+        max_shots = 100_000_000,
+        max_errors = 10,
         tasks = tasks,
         decoders=['bposd'],
         # existing_data_filepaths = existing,
