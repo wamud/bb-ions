2e7ccb6f0e0de8e853b25545dec99b0b683c3553
Anthony O'Rourke <anthony.orourke@student.uts.edu.au>
2026-04-29 15:25:22 +1000

    moved packages to packages directory; changed reference to deltakit_stim to just be stim so all future imports of stim will actually import deltakit_stim

--- scripts/generate_helios_circuits.py
+++ scripts/generate_helios_circuits.py
@@ -1,23 +1,30 @@
 # This file generates BB code circuits with the syndrome extraction circuits as usual (i.e. following Edwin Tham, Min Ye ... Delfosse "modules" paper's Algorithm 2) but with the noise model based on Helios' quantum computer.
 
 
-import stim
+
 import sys
 import os
+import deltakit_stim
+
+# Save deltakit_stim as stim so any other calls to import stim (including from other packages) just use deltakit_stim:
+injections = ['stim._detect_machine_architecture',
+'stim._stim_polyfill',
+'stim']
+for namespace in injections:
+    sys.modules[namespace] = sys.modules["deltakit_{namespace}".format(namespace=namespace)]
+
 sys.path.append(os.path.abspath("../src"))
 from bb_ions import *
 
 
-ps = [0.001, 0.002, 0.003, 0.004, 0.005, 0.006]
+ps = [0.001]
 seq_gates = True 
 exclude_opp_basis_detectors = True  # If set to false then it includes detectors on X (Z) stabiliser measurement results during Memory Z (X) -- i.e. allows correlated decoding
 swap_LRC = False
 
 
-for code in [bb6_60_8_6_one(), bb6_60_8_6_two(), bb6_60_8_6_three(), bb6_60_8_6_four(), bb6_60_8_6_five(), bb6_60_8_6_six(), 
-            bb6_56_6_8_one(), bb6_56_6_8_two(), bb6_56_6_8_three(), bb6_56_6_8_four(), bb6_56_6_8_five(), bb6_56_6_8_six(), bb6_56_6_8_seven()
-            ]:
-    print(f"{code.n}, {code.k}, {code.d_max}")
+for code in [bb6_60_8_6_one()]:
+    print(f"[[{code.n}, {code.k}, {code.d_max}]]")
     
     for memory_basis in 'X':
 
@@ -33,7 +40,7 @@ for code in [bb6_60_8_6_one(), bb6_60_8_6_two(), bb6_60_8_6_three(), bb6_60_8_6_
                 memory_basis = memory_basis, 
                 num_syndrome_extraction_cycles = num_syndrome_extraction_cycles,
                 errors = helios_errors(p),
-                idle_during = helios_idle_errors(),
+                idle_during = helios_idle_errors(p),
                 sequential_gates = seq_gates, 
                 exclude_opposite_basis_detectors = exclude_opp_basis_detectors,
                 only_CZs = True,
@@ -45,7 +52,7 @@ for code in [bb6_60_8_6_one(), bb6_60_8_6_two(), bb6_60_8_6_three(), bb6_60_8_6_
 
             filename = f"nkd=[[{code.n}_{code.k}_{code.d_max}]],p={p},noise={noise},r={num_syndrome_extraction_cycles},seq_gates={seq_gates},b={memory_basis},excl_opp_b_detectors={exclude_opp_basis_detectors},swap_LRC={swap_LRC},l={code.l},m={code.m},A='{''.join(str(x) + str(y) for x, y in code.Aij)}',B='{''.join(str(x) + str(y) for x, y in code.Bij)}'"
             
-            circuit.to_file(f"../circuits/{noise}/{filename}.stim")
+            circuit.to_file(f"../examples/example_circuits/{filename}.stim")
 
 
 
