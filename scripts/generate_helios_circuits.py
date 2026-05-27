d7616d2ec016c2a33fd4fbf524abf284745a5568
Anthony O'Rourke <anthony.orourke@student.uts.edu.au>
2026-05-27 14:06:13 +1000

    added gross code circuits with leakage heralds

--- scripts/generate_helios_circuits.py
+++ scripts/generate_helios_circuits.py
@@ -27,9 +27,13 @@ leakage_heralds = True
 
 loss_heralds = False
 
-for ps in [0.001, 0.002]:
+for p in [0.001, 0.002]:
     for swap_LRC in [True, False]:
         for leakage in [True, False]:
+
+            filename = f"n={code.n},k={code.k},d={code.d_max},p={p},noise={noise},leakage={leakage},leakage_heralds={leakage_heralds},loss={loss},loss_heralds={loss_heralds},r={num_syndrome_extraction_cycles},seq_gates={seq_gates},b={memory_basis},excl_opp_b_detectors={exclude_opp_basis_detectors},swap_LRC={swap_LRC},l={code.l},m={code.m},A='{''.join(str(x) + str(y) for x, y in code.Aij)}',B='{''.join(str(x) + str(y) for x, y in code.Bij)}'"
+            
+            print(filename)
 
             circuit = make_BB_circuit(  # see src/bb_ions/circfuncs for explanation of make_BB_circuit inputs
                 code,
@@ -53,7 +57,4 @@ for ps in [0.001, 0.002]:
             # with open(f"scrap.svg", "w", encoding="utf-8") as f: f.write(svg_string)
 
             # Save circuit:
-
-            filename = f"n={code.n},k={code.k},d={code.d_max},p={p},noise={noise},leakage={leakage},leakage_heralds={leakage_heralds},loss={loss},loss_heralds={loss_heralds},r={num_syndrome_extraction_cycles},seq_gates={seq_gates},b={memory_basis},excl_opp_b_detectors={exclude_opp_basis_detectors},swap_LRC={swap_LRC},l={code.l},m={code.m},A='{''.join(str(x) + str(y) for x, y in code.Aij)}',B='{''.join(str(x) + str(y) for x, y in code.Bij)}'"
-            print(filename)
             circuit.to_file(f"../circuits/with_leakage/helios/{filename}.stim")
