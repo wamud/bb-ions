5e40343741c52759684a0dcd74603c03763ee31b
Anthony O'Rourke <anthony.orourke@student.uts.edu.au>
2026-06-03 16:40:59 +1000

    minor edits

--- scripts/generate_helios_circuits.py
+++ scripts/generate_helios_circuits.py
@@ -23,13 +23,13 @@ num_syndrome_extraction_cycles = 12
 seq_gates = True
 exclude_opp_basis_detectors = True
 leakage_heralds = False
-leakage = True
+leakage = False
 swap_LRC = False
 loss_heralds = False
 only_CZs = True
 
-for p in [0.001, 0.002]:
-        filename = f"n={code.n},k={code.k},d={code.d_max},p={p},noise={noise},leakage={leakage},leakage_repumping=enter_manually,leakage_heralds={leakage_heralds},loss={loss},loss_heralds={loss_heralds},r={num_syndrome_extraction_cycles},seq_gates={seq_gates},b={memory_basis},excl_opp_b_detectors={exclude_opp_basis_detectors},swap_LRC={swap_LRC},l={code.l},m={code.m},A='{''.join(str(x) + str(y) for x, y in code.Aij)}',B='{''.join(str(x) + str(y) for x, y in code.Bij)}'"
+for p in [0.001]:
+        filename = f"n={code.n},k={code.k},d={code.d_max},p={p},noise={noise},leakage={leakage},leakage_repumping=False,leakage_heralds={leakage_heralds},loss={loss},loss_heralds={loss_heralds},r={num_syndrome_extraction_cycles},seq_gates={seq_gates},b={memory_basis},excl_opp_b_detectors={exclude_opp_basis_detectors},swap_LRC={swap_LRC},l={code.l},m={code.m},A='{''.join(str(x) + str(y) for x, y in code.Aij)}',B='{''.join(str(x) + str(y) for x, y in code.Bij)}'"
         
         print(filename)
 
