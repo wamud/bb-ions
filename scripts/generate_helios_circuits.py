451f2d110f683a3254fde33d1ae1eb23b2fc2a62
Anthony O'Rourke <anthony.orourke@student.uts.edu.au>
2026-06-10 14:03:16 +1000

    deleted un-needed circuits

--- scripts/generate_helios_circuits.py
+++ scripts/generate_helios_circuits.py
@@ -15,7 +15,7 @@ sys.path.append(os.path.abspath("../src"))
 from bb_ions import *
 
 
-code = gross_code()
+code = two_gross_code()
 memory_basis = 'X'
 noise = 'helios'
 loss = False
@@ -24,40 +24,40 @@ seq_gates = True
 exclude_opp_basis_detectors = True
 leakage_heralds = False
 leakage = True
-# swap_LRC = False
+swap_LRC = False
 loss_heralds = False
 only_CZs = True
 leakage_repumping = False
 
-for swap_LRC in [True, False]:
-    for p in [0.001]:
-        for cycles in [0]:
-            filename = f"n={code.n},k={code.k},d={code.d_max},p={p},noise={noise},leakage={leakage},leakage_repumping={leakage_repumping},repumping_cycles={cycles},leakage_heralds={leakage_heralds},loss={loss},loss_heralds={loss_heralds},r={num_syndrome_extraction_cycles},seq_gates={seq_gates},b={memory_basis},excl_opp_b_detectors={exclude_opp_basis_detectors},swap_LRC={swap_LRC},l={code.l},m={code.m},A='{''.join(str(x) + str(y) for x, y in code.Aij)}',B='{''.join(str(x) + str(y) for x, y in code.Bij)}'"
-            
-            print(filename)
 
-            circuit = make_BB_circuit(  # see src/bb_ions/circfuncs for explanation of make_BB_circuit inputs
-                code,
-                p,  
-                errors = helios_errors(p),
-                idle_during = helios_idle_errors(p),
-                num_syndrome_extraction_cycles = num_syndrome_extraction_cycles,  
-                memory_basis = memory_basis,
-                sequential_gates = seq_gates, 
-                exclude_opposite_basis_detectors = exclude_opp_basis_detectors,
-                reuse_check_qubits = True,
-                leakage = leakage,
-                leakage_heralds = leakage_heralds,
-                swap_LRC = swap_LRC,
-                only_CZs = only_CZs,
-                leakage_repumping = leakage_repumping,
-                num_repumping_cycles = cycles,
-            )
+for p in [0.001]:
+    for cycles in [0]: #range(1,11):
+        filename = f"n={code.n},k={code.k},d={code.d_max},p={p},noise={noise},leakage=DEP1,leakage_repumping={leakage_repumping},repumping_cycles={cycles},leakage_heralds={leakage_heralds},loss={loss},loss_heralds={loss_heralds},r={num_syndrome_extraction_cycles},seq_gates={seq_gates},b={memory_basis},excl_opp_b_detectors={exclude_opp_basis_detectors},swap_LRC={swap_LRC},l={code.l},m={code.m},A='{''.join(str(x) + str(y) for x, y in code.Aij)}',B='{''.join(str(x) + str(y) for x, y in code.Bij)}'"
+        
+        print(filename)
 
-            # svg = circuit.without_noise().diagram('timeline-svg') # diagram without noise
-            # display(svg)
-            # svg_string = str(svg)
-            # with open(f"scrap.svg", "w", encoding="utf-8") as f: f.write(svg_string)
+        circuit = make_BB_circuit(  # see src/bb_ions/circfuncs for explanation of make_BB_circuit inputs
+            code,
+            p,  
+            errors = helios_errors(p),
+            idle_during = helios_idle_errors(p),
+            num_syndrome_extraction_cycles = num_syndrome_extraction_cycles,  
+            memory_basis = memory_basis,
+            sequential_gates = seq_gates, 
+            exclude_opposite_basis_detectors = exclude_opp_basis_detectors,
+            reuse_check_qubits = True,
+            leakage = leakage,
+            leakage_heralds = leakage_heralds,
+            swap_LRC = swap_LRC,
+            only_CZs = only_CZs,
+            leakage_repumping = leakage_repumping,
+            num_repumping_cycles = cycles,
+        )
 
-            # Save circuit:
-            circuit.to_file(f"../circuits/with_leakage/helios/leakage_repumping_before_gates/{filename}.stim")
+        # svg = circuit.without_noise().diagram('timeline-svg') # diagram without noise
+        # display(svg)
+        # svg_string = str(svg)
+        # with open(f"scrap.svg", "w", encoding="utf-8") as f: f.write(svg_string)
+
+        # Save circuit:
+        circuit.to_file(f"../circuits/with_leakage/helios/leakage_repumping_before_gates/{filename}.stim")
