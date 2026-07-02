a31d1da84b8884d608c173ce2982f673d45510c0
wamud <anthony.orourke@student.uts.edu.au>
2026-07-02 14:13:30 +1000

    added some codes and scripts to make the big ones in parallel

--- scripts/generate_helios_756.py
+++ scripts/generate_helios_756.py
@@ -31,7 +31,7 @@ p = 0.001
 
 
 
-for code in [gross_code(), two_gross_code()]:
+for code in [bb6_756_code()]:
     filename = f"n={code.n},k={code.k},d={code.d_max},p={p},noise={noise},leakage={leakage},loss={loss},leakage_heralds={leakage_heralds},leakage_repumping={leakage_repumping},repumping_cycles={cycles},swapLRC={swapLRC},rounds={num_syndrome_extraction_cycles},seq_ops={seq_ops},seq_meas={seq_meas},b={memory_basis},excl_opp_detectors={exclude_opp_basis_detectors},l={code.l},m={code.m},A={'+'.join(f'x{i}y{j}' for i, j in code.Aij)},B={'+'.join(f'x{i}y{j}' for i, j in code.Bij)}"
     
     print("Creating: ",filename)
