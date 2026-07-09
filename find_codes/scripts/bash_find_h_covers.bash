4cd0e336d416e20ce8633f959df208579cd13835
wamud <nawaran1@outlook.com>
2026-07-09 11:52:04 +1000

    minor edits; added iHPC stats

--- find_codes/scripts/bash_find_h_covers.bash
+++ find_codes/scripts/bash_find_h_covers.bash
@@ -2,11 +2,11 @@ source /home/aforourk/Data/anaconda3/etc/profile.d/conda.sh
 conda activate base
 
 for h in {2..5}; do
-    python find_h_cover_codes.py "$h" bb6_72_12_6 16 18 &
+    python find_h_cover_codes.py bb6_72_12_6 "$h" 16 18 &
 done
 
 for h in {6..10}; do
-    python find_h_cover_codes.py "$h" bb6_72_12_6 20 24 &
+    python find_h_cover_codes.py bb6_72_12_6 "$h" 20 24 &
 done
 
 
