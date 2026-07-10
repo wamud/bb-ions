ca1794bbefa42c6cae28c8f01d1e13dc8da51b6c
Anthony O'Rourke <anthony.orourke@student.uts.edu.au>
2026-07-10 10:26:30 +1000

    added some circuits; renamed find_circuit_level_distance directory

--- find_codes/scripts/bash_find_h_covers.bash
+++ find_codes/scripts/bash_find_h_covers.bash
@@ -2,12 +2,10 @@ source /home/aforourk/Data/anaconda3/etc/profile.d/conda.sh
 conda activate base
 
 for h in {2..5}; do
-    python find_h_cover_codes.py bb6_72_12_6 "$h" 16 18 &
+    python find_h_cover_codes.py bb5_30_4_5 "$h" 10 10 &
 done
-
 for h in {6..10}; do
-    python find_h_cover_codes.py bb6_72_12_6 "$h" 20 24 &
+    python find_h_cover_codes.py bb6_72_12_6 "$h" 10 10 &
 done
-
 
 wait
